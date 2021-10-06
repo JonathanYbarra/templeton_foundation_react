75236f5250aadc2d3330d8a68da2b8c94968d82e
jybarra <jybarra@technisys.com>
2021-10-06 18:05:04 -0300

    actualizacion de activipies

--- src/core/services/activities.js
+++ src/core/services/activities.js
@@ -1,12 +1,57 @@
 export const monthlySeminars = [
   {
     id: 1,
-    pdfPresentation: "/pdfs/activities/pruebaPDF.pdf",
-    title: "La Flecha del Tiempo en Cosmología",
+    pdfPresentation: "/pdfs/activities/presentacion1.pdf",
+    title: "Wandering thoughts on symmetries",
     description:
-      "En cosmología, suele denominarse “flecha cosmológica del tiempo” al sentido temporal de expansión del universo: dicha flecha será global en un universo Big Bang-Big Chill, pero apuntará en distintos sentidos en un universo Big Bang-Big Crunch. Esta terminología privilegia el “radio” del universo frente a otras propiedades geométricas del espacio-tiempo. Pero, ¿por qué aceptar el carácter privilegiado del radio respecto de otras propiedades geométricas del universo? En el presente trabajo se argumentará que: (i) La flecha del tiempo cosmológica no es una consecuencia inevitable de las leyes físicas: es una propiedad contingente del espacio-tiempo, (ii) los modelos cosmológicos standard de la cosmología contemporánea cumplen las condiciones necesarias para posee una flecha cosmológica del tiempo con alta probabilidad, y (iii) el carácter contingente de la flecha cosmológica del tiempo no minimiza su carácter objetivo.",
+      "En el presente trabajo analizaré algunos aspectos de las simetrías en física. En particular, argumentaré que la interpretación de las simetrías en física depende fuertemente de: (i) qué se entiende por teoría física y ley, (ii) qué papel cumple la idealización en física y en metafísica, y (iii) que ontología subyacente se presupone.",
+    directedBy: "Seminar dictated by Cristian Lopez",
+    date: "17 de Marzo de 2021",
+  },
+  {
+    id: 2,
+    pdfPresentation: "/pdfs/activities/presentacion2.pdf",
+    title: "La interpretación modal-Hamiltoniana y la naturaleza relacional del tiempo",
+    description:
+      "Sobre la base de la distinción entre tiempo externo y tiempo de eventos en mecánica cuántica, se propone una reconstrucción relacional del tiempo de eventos sobre la base de la interpretación modal-Hamiltoniana. Se señalará, además, que si se adopta una perspectiva holista que no distinga, el sistema completo resulta atemporal: no hay secuencia de eventos.",
+    directedBy: "Seminar dictated by Matías Pasqualini and Sebastian Fortin",
+    date: "14 de Abril de 2021",
+  },
+  {
+    id: 3,
+    pdfPresentation: "/pdfs/activities/presentacion3.pdf",
+    title: "Revisión del artículo de De Haro ‘Noether’s Theorems and Energy in General Relativity’",
+    description:
+      "La presentación se abocará a revisar y comentar el artículo de Sebastian De Haro “Noether’s Theorems and Energy in General Relativity” (2021). En particular, se considerará el modo en que De Haro evalúa diferentes propuestas respecto de (i) la definición de energía (momentum) gravitacional y (ii) la conservación de la energía en relatividad general.",
+    directedBy: "Seminar dictated by Manuel Herrera and Cecilia Bejarano",
+    date: "19 de Mayo de 2021",
+  },
+  {
+    id: 4,
+    pdfPresentation: "/pdfs/activities/presentacion4.pdf",
+    title: "El enfoque global-geométrico de la flecha del tiempo: revisando sus requisitos",
+    description:
+      "El problema de la flecha del tiempo consiste en brindar un fundamento físico a la diferencia entre las dos direcciones temporales. La respuesta tradicional apela al segundo principio de la termodinámica: la dirección pasado-a-futuro se concibe como la dirección en la que aumenta la entropía. El propósito de esta charla será presentar un enfoque diferente, según el cual la flecha del tiempo puede definirse en términos de las propiedades geométricas del espacio-tiempo, independientemente de argumentos termodinámicos. Desde esta perspectiva, la flecha del tiempo es una característica contingente que el universo posee si su espacio-tiempo cumple con los siguientes requisitos: orientabilidad temporal, existencia de tiempo global y asimetría temporal. A su vez, se analizarán las consecuencias de relajar cada uno de esos requisitos.",
+    directedBy: "Seminar dictated by Olimpia Lombardi and Cecilia Bejarano",
+    date: "16 de Junio de 2021",
+  },
+  {
+    id: 5,
+    pdfPresentation: "/pdfs/activities/presentacion5.pdf",
+    title: "El problema de la irreversibilidad: primera parte",
+    description:
+      "El problema de la flecha del tiempo consiste en brindar un fundamento físico a la diferencia entre las dos direcciones temporales. La respuesta tradicional apela al segundo principio de la termodinámica: la dirección pasado-a-futuro se concibe como la dirección en la que aumenta la entropía. El propósito de esta charla será presentar un enfoque diferente, según el cual la flecha del tiempo puede definirse en términos de las propiedades geométricas del espacio-tiempo, independientemente de argumentos termodinámicos. Desde esta perspectiva, la flecha del tiempo es una característica contingente que el universo posee si su espacio-tiempo cumple con los siguientes requisitos: orientabilidad temporal, existencia de tiempo global y asimetría temporal. A su vez, se analizarán las consecuencias de relajar cada uno de esos requisitos.",
     directedBy: "Seminar dictated by Olimpia Lombardi",
-    date: "7 de Abril de 2021",
+    date: "11 de Agosto de 2021",
+  },
+  {
+    id: 6,
+    pdfPresentation: "/pdfs/activities/presentacion6.pdf",
+    title: "El problema de la irreversibilidad: segunda parte",
+    description:
+      "Este trabajo, continuación del anterior, se focalizará en explicar el programa de Boltzmann de reducción de la termodinámica a la mecánica. Sobre esta base, se recordarán las críticas que dicho programa debió enfrentar, en particular las de Loschmidt y Zermelo. Finalmente, se introducirá brevemente el enfoque de Gibbs, con la idea de dejar preparado el terreno para discutir, en próximos encuentros, ambos enfoques desde una perspectiva ya no histórica sino técnica y filosófica.",
+    directedBy: "Seminar dictated by Olimpia Lombardi",
+    date: "1 de Septiembre de 2021",
   },
 ];
 
--- src/core/services/presentationsAcademic.js
+++ src/core/services/presentationsAcademic.js
@@ -1,21 +1,29 @@
 export const contentValues = [
   {
     key: 1,
-    boldText: "“Three facets of time reversal”, ",
-    text: `by Cristian López. Invited Speaker at the Université Catholique de Louvain, December 2 (2020).`,
+    boldText: "“Against symmetry fundamentalism, in favor of symmetry deflationism”, ",
+    text: `Cristian López (2021). Invited speaker at the Austro-Belgian Conference on Philosophy of Science and Physics, March 17`,
     marginTop: "mt-5",
   },
   {
     key: 2,
     boldText: "“A cosmological, geometric approach to the arrow of time” ",
-    text: `by Olimpia Lombardi and Cristian López. Invited speakers at the Weekly Proteus Seminar Series, University of Barcelona, online, February 23 (2021).`,
+    text: `Olimpia Lombardi and Cristian López (2021).Invited speakers at the Weekly Proteus Seminar Series, University of Barcelona, February 23`,
     marginTop: "mt-1",
   },
   {
     key: 3,
     boldText:
-      "“Against symmetry fundamentalism, in favor of symmetry deflationism” ",
-    text: `by Cristian López. Invited speaker at the Austro-Belgian Conference on Philosophy of Science and Physics, online, March 17 (2021).`,
+      "“Three facets of time reversal” ",
+    text: `by Cristian Lopez (2021).Invited Speaker at the Université Catholique de Louvain, December 2.`,
     marginTop: "mt-1",
   },
+  {
+    key: 4,
+    boldText:
+      "“Time’s direction at the Planck scale” ",
+    text: `by Cristian López (2021). Talk given at the East European Network for Philosophy of Science Conference (EENPS 2021), June 10.`,
+    marginTop: "mt-1",
+  },
+ 
 ];
